ea73647afa13b790f7a501bf8c63f5344b57b56f
Lorenzo <lorenzo.zem8065@mediadesign.school.nz>
2021-07-03 11:47:53 +1200

    Initial Commit

---
+++ SubTrack/SubTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubTrackApp.swift
+//  SubTrack
+//
+//  Created by Lorenzo Zemp on 3/07/21.
+//
+
+import SwiftUI
+
+@main
+struct SubTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
